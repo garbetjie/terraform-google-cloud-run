081209624afe0d2eccad1952b82d5ed25fc7f759
Geoff Garbers <geoff@garbers.co.za>
2021-06-24 17:45:10 +0200

    * Add secret aliases to environment variables populated by a secret. * Populate volume objects. * Create map of secrets to aliases for any secrets that appear to come from another project.

--- locals.tf
+++ locals.tf
@@ -2,12 +2,47 @@ locals {
   project_id = var.project_id
 
   # Fill in defaults for environment variables.
-  env = [
+  env = toset([
     for e in var.env: {
       env = e.env
       value = e.value
-      secret = e.secret
+      secret = {
+        name = e.secret
+        alias = e.secret != null ? lookup(local.secrets_to_aliases, e.secret, null) : null
+      }
       version = coalesce(e.version, "latest")
     }
-  ]
+  ])
+
+  // Fill in defaults for volumes.
+  volumes = toset([
+    for vol in var.volumes: {
+      path = vol.path
+      name = "volume-${substr(sha1(jsonencode(vol)), 0, 4)}"
+      secret = {
+        name = vol.secret
+        alias = lookup(local.secrets_to_aliases, vol.secret, null)
+      }
+      items = [
+        for filename, version in coalesce(vol.versions, { latest = "latest" }): {
+          filename = filename,
+          version = version
+        }
+      ]
+    }
+  ])
+
+  // Map secrets in other projects to aliases. This allows for easy lookup when building up `local.env` and `local.volumes`.
+  secrets_to_aliases = {
+    for secret in distinct(concat(
+        [for env in var.env: env.secret if env.secret != null],
+        [for vol in var.volumes: vol.secret if vol.secret != null]
+      )):
+      secret => "secret-${substr(sha1(secret), 0, 4)}"
+    if length(split("/", secret)) > 1
+  }
+
+  // It seems like a BETA launch stage is still okay for functionality in PREVIEW.
+//  launch_stage = length(local.volumes) > 0 || local.env_from_secrets_count > 0 ? "BETA" : "BETA"
+  launch_stage = "BETA"
 }
