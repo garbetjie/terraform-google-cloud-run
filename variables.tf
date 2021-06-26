0dfbc9bb8dac606417d2eca34dd0d967527a5867
Geoff Garbers <geoff@garbers.co.za>
2021-06-26 13:38:02 +0200

    Update input arguments in README, and re-order input variables to be alphabetical.

--- variables.tf
+++ variables.tf
@@ -13,52 +13,22 @@ variable location {
   description = "Location in which to run the service."
 }
 
-variable labels {
-  type = map(string)
-  default = {}
-  description = "Labels to apply to the service."
-}
-
 variable allow_public_access {
   type = bool
   default = true
   description = "Allow non-authenticated access to the service."
 }
 
-variable memory {
-  type = number
-  default = 256
-  description = "Memory (in MB) to allocate to service instances."
-}
-
-variable project {
-  type = string
-  default = null
-  description = "Google Cloud project in which to create the service."
-}
-
-variable cpus {
-  type = number
-  default = 1
-  description = "CPUs to allocate to service instances."
+variable args {
+  type = list(string)
+  default = []
+  description = "Arguments to the service's entrypoint."
 }
 
 variable cloudsql_connections {
-  type = list(string)
+  type = set(string)
   default = []
   description = "Cloud SQL connections to attach to service instances."
-}
-
-variable revision {
-  type = string
-  default = null
-  description = "Revision name to create and deploy."
-}
-
-variable timeout {
-  type = number
-  default = 60
-  description = "Length of time (in seconds) to allow requests to run for."
 }
 
 variable concurrency {
@@ -67,28 +37,16 @@ variable concurrency {
   description = "Maximum number of requests a single service instance can handle at once."
 }
 
-variable max_instances {
+variable cpus {
   type = number
-  default = 1000
-  description = "Maximum number of service instances to allow to start."
+  default = 1
+  description = "CPUs to allocate to service instances."
 }
 
-variable min_instances {
-  type = number
-  default = 0
-  description = "Minimum number of service instances to keep running."
-}
-
-variable service_account_email {
-  type = string
-  default = null
-  description = "Service account email to assign to the service."
-}
-
-variable map_domains {
-  type = set(string)
+variable entrypoint {
+  type = list(string)
   default = []
-  description = "Domain names to map to this service instance."
+  description = "Entrypoint command. Defaults to the image's ENTRYPOINT if not provided."
 }
 
 variable env {
@@ -113,16 +71,75 @@ variable env {
   }
 }
 
-variable args {
-  type = list(string)
-  default = []
-  description = "Arguments to the service's entrypoint."
+variable ingress {
+  type = string
+  default = "all"
+  description = "Restrict network access to this service. Allowed values: [`all`, `internal`, `internal-and-cloud-load-balancing`]"
+
+  validation {
+    error_message = "Ingress must be one of: [\"all\", \"internal\", \"internal-and-cloud-load-balancing\"]."
+    condition = contains(["all", "internal", "internal-and-cloud-load-balancing"], var.ingress)
+  }
 }
 
-variable entrypoint {
-  type = list(string)
+variable labels {
+  type = map(string)
+  default = {}
+  description = "Labels to apply to the service."
+}
+
+variable map_domains {
+  type = set(string)
   default = []
-  description = "Entrypoint command. Defaults to the image's ENTRYPOINT if not provided."
+  description = "Domain names to map to this service instance."
+}
+
+variable max_instances {
+  type = number
+  default = 1000
+  description = "Maximum number of service instances to allow to start."
+}
+
+variable memory {
+  type = number
+  default = 256
+  description = "Memory (in MB) to allocate to service instances."
+}
+
+variable min_instances {
+  type = number
+  default = 0
+  description = "Minimum number of service instances to keep running."
+}
+
+variable port {
+  type = number
+  default = 8080
+  description = "Port on which the container is listening for incoming HTTP requests."
+}
+
+variable project {
+  type = string
+  default = null
+  description = "Google Cloud project in which to create the service."
+}
+
+variable revision {
+  type = string
+  default = null
+  description = "Revision name to create and deploy."
+}
+
+variable service_account_email {
+  type = string
+  default = null
+  description = "Service account email to assign to the service."
+}
+
+variable timeout {
+  type = number
+  default = 60
+  description = "Length of time (in seconds) to allow requests to run for."
 }
 
 variable volumes {
@@ -157,21 +174,4 @@ variable vpc_access_egress {
   type = string
   default = "private-ranges-only"
   description = "Specify whether to divert all outbound traffic through the VPC, or private ranges only (Deprecated - use `var.vpc_access.egress` instead)."
-}
-
-variable port {
-  type = number
-  default = 8080
-  description = "Port on which the container is listening for incoming HTTP requests."
-}
-
-variable ingress {
-  type = string
-  default = "all"
-  description = "Restrict network access to this service. Allowed values: [`all`, `internal`, `internal-and-cloud-load-balancing`]"
-
-  validation {
-    error_message = "Ingress must be one of: [\"all\", \"internal\", \"internal-and-cloud-load-balancing\"]."
-    condition = contains(["all", "internal", "internal-and-cloud-load-balancing"], var.ingress)
-  }
 }
